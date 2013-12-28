e85af0168e9105bd7c3da1117cea0fc2c2685b86
ShiZhan <g.shizhan.g@gmail.com>
2013-12-28 13:28:54 -0600

    case class encapsulation

--- src/main/scala/clairvoyant.scala
+++ src/main/scala/clairvoyant.scala
@@ -38,17 +38,13 @@ object Hash {
       .map("%02x".format(_)).mkString
 }
 
-case class Page(uri: String, content: String) {
+case class Page(url: String, content: String) {
   import java.io.{ File, PrintWriter }
-  import java.security.MessageDigest
-  import Hash.md5
-
-  val uriHash = md5(uri)
 
   def storeTo(folder: String) = {
-    val file = new File(folder + "/" + uriHash + ".html")
+    val file = new File(folder + "/" + Hash.md5(url) + ".html")
     val writer = new PrintWriter(file)
-    writer.println("<!-- " + uri + " -->")
+    writer.println("<!-- " + url + " -->")
     writer.println(content)
     writer.close
   }
@@ -60,28 +56,23 @@ case class Link(links: List[String]) {
 
 case class STOP
 
-object Parser {
-  import scala.collection.JavaConversions._
-  import collection.mutable.HashSet
-  import java.net.URL
-  import org.jsoup.Jsoup
+case class Parse(url: String) {
+  import collection.JavaConversions._
 
-  private var traveled = HashSet[String]()
-  def crawled(url: String) = traveled.contains(url)
-  def allURLs = traveled.iterator
+  private val doc = org.jsoup.Jsoup.connect(url).get
 
-  def urlValid(url: String) = {
+  private def urlValid(url: String) = {
     try {
-      new URL(url)
+      new java.net.URL(url)
       true
     } catch {
       case _: Exception => false
     }
   }
 
-  def readPage(url: String) = {
-    val doc = Jsoup.connect(url).get
-    // TODO: a json configuration file is required
+  def getPage = Page(url, doc.html)
+
+  def getLink = {
     val area =
       if (url == """http://shizhan.github.io/archive.html""")
         doc.select("div.content")
@@ -89,42 +80,93 @@ object Parser {
         doc.select("div.page-header")
     val links =
       area.select("a").iterator.map(_.attr("abs:href")).filter(urlValid).toList
-    traveled += url
-    (Page(url, doc.html), Link(links))
-  }
-}
-
-object Console {
-  val prompt = "> "
-
-  def console: Unit = {
-    print(prompt)
-    for (line <- io.Source.stdin.getLines) {
-      val output = line.split(" ").toList match {
-        case "exit" :: Nil => return
-        case "" :: Nil => null
-        case _ => "Unrecognized command: [%s]".format(line)
-      }
-      if (null != output) println(output)
-      print(prompt)
-    }
+    Link(links)
   }
 }
 
 object Spider extends Logging {
   import java.io.File
   import io.Source
+  import collection.mutable.HashSet
   import util.parsing.json.JSON
+  import actors.Actor
   import actors.Actor._
-  import Parser.{ allURLs, crawled, readPage }
 
-  case class SpiderConfig(
+  case class SpiderInstance(
+    writer: Actor,
+    loaders: Seq[Actor],
+    controller: Actor) {
+    def stop = {
+      controller ! STOP
+      loaders.foreach(_ ! STOP)
+      writer ! STOP
+
+      logger.info("STOP signal has been sent to all actors ...")
+    }
+  }
+
+  case class Spider(
     startURLs: List[String],
     concurrency: Int,
     filters: List[(String, String)],
-    folder: String)
+    folder: String) {
+    private var traveled = HashSet[String]()
+    def crawled(url: String) = traveled.contains(url)
+    def allURLs = traveled.iterator
 
-  def load(fileName: String) = {
+    def run = {
+      val writer = actor {
+        loop {
+          react {
+            case page: Page => page.storeTo(folder)
+            case STOP => exit
+          }
+        }
+      }
+
+      val loaders = (0 to concurrency - 1) map { i =>
+        actor {
+          loop {
+            react {
+              case url: String => {
+                logger.info("Loader [{}]: {}", i, url)
+
+                try {
+                  val result = Parse(url)
+                  val page = result.getPage
+                  val links = result.getLink
+                  writer ! page
+                  if (!links.isEmpty) sender ! links
+                  traveled += url
+                } catch {
+                  case e: Exception => println(e)
+                }
+              }
+              case STOP => exit
+            }
+          }
+        }
+      }
+
+      val controller = actor {
+        loop {
+          react {
+            case Link(urls) =>
+              urls.filter(u => !crawled(u)).grouped(concurrency).foreach {
+                _.zipWithIndex.foreach { case (url, index) => loaders(index) ! url }
+              }
+            case STOP => exit
+          }
+        }
+      }
+
+      controller ! Link(startURLs)
+
+      SpiderInstance(writer, loaders, controller)
+    }
+  }
+
+  def initialize(fileName: String) = {
     try {
       val fileContent = Source.fromFile(new File(fileName)).mkString
       val config = JSON.parseFull(fileContent).get.asInstanceOf[Map[String, Any]]
@@ -142,74 +184,30 @@ object Spider extends Logging {
           _folder.mkdir
           _name
         }
-      SpiderConfig(startURLs, concurrency, filters, folder)
+      Spider(startURLs, concurrency, filters, folder)
     } catch {
       case e: Exception => {
         println(e)
-        SpiderConfig(List[String](), 1, List(("", "")), "")
+        Spider(List[String](), 1, List(("", "")), "")
       }
     }
   }
+}
 
-  case class SpiderInstance(
-    writer: actors.Actor,
-    loaders: Seq[actors.Actor],
-    controller: actors.Actor)
+object Console {
+  val prompt = "> "
 
-  def run(config: SpiderConfig) = {
-    val writer = actor {
-      loop {
-        react {
-          case page: Page => page.storeTo(config.folder)
-          case STOP => exit
-        }
+  def console: Unit = {
+    print(prompt)
+    for (line <- io.Source.stdin.getLines) {
+      val output = line.split(" ").toList match {
+        case "exit" :: Nil => return
+        case "" :: Nil => null
+        case _ => "Unrecognized command: [%s]".format(line)
       }
+      if (null != output) println(output)
+      print(prompt)
     }
-
-    val loaders = (0 to config.concurrency - 1) map { i =>
-      actor {
-        loop {
-          react {
-            case url: String => {
-              logger.info("Loader [{}]: {}", i, url)
-
-              try {
-                val (page, links) = readPage(url)
-                writer ! page
-                if (!links.isEmpty) sender ! links
-              } catch {
-                case e: Exception => println(e)
-              }
-            }
-            case STOP => exit
-          }
-        }
-      }
-    }
-
-    val controller = actor {
-      loop {
-        react {
-          case Link(urls) => urls.filter(u => !crawled(u)).grouped(config.concurrency)
-            .foreach {
-              _.zipWithIndex.foreach { case (url, index) => loaders(index) ! url }
-            }
-          case STOP => exit
-        }
-      }
-    }
-
-    controller ! Link(config.startURLs)
-
-    SpiderInstance(writer, loaders, controller)
-  }
-
-  def stop(spider: SpiderInstance) = {
-    val SpiderInstance(writer, loaders, controller) = spider
-    controller ! STOP
-    loaders.foreach(_ ! STOP)
-    writer ! STOP
-    logger.info("Traveled URI: [{}]", allURLs.length)
   }
 }
 
@@ -231,11 +229,12 @@ spider example:
 
   def main(args: Array[String]) = if (args.length < 1) println(usage)
   else {
-    val config = Spider.load(args(0))
-    if (!config.startURLs.isEmpty) {
-      val spider = Spider.run(config)
+    val spider = Spider.initialize(args(0))
+    if (!spider.startURLs.isEmpty) {
+      val spiderInstance = spider.run
       Console.console
-      Spider.stop(spider)
+      spiderInstance.stop
+      println("Traveled URI: " + spider.allURLs.length)
     } else println("spider config file error")
   }
 }
